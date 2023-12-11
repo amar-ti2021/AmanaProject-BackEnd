03d11cc0d7ee0cccaaf57a564842393a00bfcee5
amar-ti2021 <muha21095ti@student.nurulfikri.ac.id>
2023-12-12 06:42:24 +0700

    fead: add cors

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = require("./routes/api");
+const cors = require("cors");
 const authenticateToken = require("./middlewares/authenticateToken");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(authenticateToken);
-app.use(router);
+const apiUrl = "/api/v1";
+app.use(apiUrl, router);
 
 app.listen(3000);
